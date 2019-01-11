6b9b3ac8ba5b09ccc560bb0818118716ea883a16
kouamano <amano.au1@gmail.com>
2019-01-11 23:12:32 +0900

    rev

--- compile_all.n007.sh
+++ compile_all.n007.sh
@@ -24,63 +24,6 @@ gcc44 y.tab.c lex.yy.c -lfl -ll
 mv a.out eet
 mv y.output eet.output
 
-##eetR
-echo eetR
-lex t.l
-yacc -dv eetR.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out eetR
-mv y.output eetR.output
-
-##eetRL
-echo eetRL
-lex t.l
-yacc -dv eetRL.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out eetRL
-mv y.output eetRL.output
-
-
-##mlf
-echo mlf
-lex mlf.l
-yacc -dv mlf.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out mlf
-mv y.output mlf.output
-
-##mlf_L
-echo mlf_L
-lex mlf_L.l
-yacc -dv mlf_L.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out mlf_L
-mv y.output mlf_L.output
-
-
-##llf
-echo llf
-lex llf.l
-yacc -dv llf.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out llf
-mv y.output llf.output
-
-##llf_meta
-echo mlf_meta
-lex llf_meta.l
-yacc -dv llf_meta.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out llf_meta
-mv y.output llf_meta.output
-
-##llf_dict
-echo llf_dict
-lex llf_dict.l
-yacc -dv llf_dict.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out llf_dict
-mv y.output llf_dict.output
 
 ##s
 echo s
@@ -106,21 +49,6 @@ gcc44 y.tab.c lex.yy.c -lfl -ll
 mv a.out ees
 mv y.output ees.output
 
-##eesR
-echo eesR
-lex s.l
-yacc -dv eesR.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out eesR
-mv y.output eesR.output
-
-##eesRL
-echo eesRL
-lex s.l
-yacc -dv eesRL.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out eesRL
-mv y.output eesRL.output
 
 # converter
 ##S2T
