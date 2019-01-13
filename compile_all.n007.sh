1024c7998c34389249213601e4880d1f9be1c9ec
kouamano <amano.au1@gmail.com>
2019-01-13 18:50:23 +0900

    rev

--- compile_all.n007.sh
+++ compile_all.n007.sh
@@ -16,13 +16,6 @@ gcc44 y.tab.c lex.yy.c -lfl -ll
 mv a.out et
 mv y.output et.output
 
-##eet
-echo eet
-lex t.l
-yacc -dv eet.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out eet
-mv y.output eet.output
 
 
 ##s
@@ -41,13 +34,6 @@ gcc44 y.tab.c lex.yy.c -lfl -ll
 mv a.out es
 mv y.output es.output
 
-##ees
-echo ees
-lex s.l
-yacc -dv ees.y
-gcc44 y.tab.c lex.yy.c -lfl -ll
-mv a.out ees
-mv y.output ees.output
 
 
 # converter
