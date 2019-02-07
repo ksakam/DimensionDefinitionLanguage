cbd88f73ef8b72c4c6b9104bfa20fa27b7b82c62
kouamano <amano.au1@gmail.com>
2019-02-07 15:09:13 +0900

    rm old code

--- mM2j.ml
+++
@@ -1,2 +0,0 @@
-#!/usr/local/bin/math -script
-Function[Print[ Quiet[   StringReplace[{"{"->"","}"->"",", "->"\n"}][ToString[Map[StringReplace[{" "->"","\t"->"","\n"->""}][ExportString[#,"ExpressionJSON"]]&,Get[#1]]]]]   ]]
